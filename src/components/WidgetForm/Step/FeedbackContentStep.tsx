3c8f1207279b6d84516ec9651f74ca4278f13bb5
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-05-07 15:52:00 -0300

    Disable send button when does not have comment

--- src/components/WidgetForm/Step/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Step/FeedbackContentStep.tsx
@@ -20,7 +20,7 @@ export function FeedbackContentStep({
 
   function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    
+
     console.log({ screenshot, comment });
   }
 
@@ -58,7 +58,8 @@ export function FeedbackContentStep({
 
           <button 
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            disabled={comment.length === 0}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500 disabled:cursor-not-allowed"
           >
             Enviar feedback
           </button>
